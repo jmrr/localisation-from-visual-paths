ba31e1861f6da0430744edda6281cd9980d160da
Jose Rivera <jose.rivera@imperial.ac.uk>
2015-10-24 21:27:18 +0100

    Removed setup

--- setup.m
+++
@@ -1,5 +0,0 @@
-% SETUP adds the paths for the necessary 3rd party libraries
-
-addpath('/home/jose/Applications/yael_matlab_linux64_v401');
-addpath('/home/jose/Applications/vlfeat-0.9.20/toolbox');
-vl_setup;
